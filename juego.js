934061fa7f5c9c085790774ee2de1b1dfdca1d1e
Ivan Madera <ivan.madera1999@gmail.com>
2020-07-15 18:54:08 -0500

    Primer avance del curso

---
+++ juego.js
@@ -0,0 +1,49 @@
+var contexto = document.getElementById("lienzoJuego").getContext("2d")
+contexto.canvas.width = 300
+contexto.canvas.height = 530
+var FPS = 60
+var gravedad = 1.5
+var personaje = {
+    x:100,
+    y:150,
+    w:50,
+    h:50
+}
+
+//IMAGENES
+var bird = new Image()
+bird.src = "imagenes/bird.png"
+
+var background = new Image()
+background.src = "imagenes/background.png"
+
+var tuberiaNorte = new Image()
+tuberiaNorte.src = "imagenes/tuberiaNorte.png"
+
+var tuberiaSur = new Image()
+tuberiaSur.src = "imagenes/tuberiaSur.png"
+
+var suelo = new Image()
+suelo.src = "imagenes/suelo.png"
+
+//CONTROL
+function presionar() {
+    personaje.y -= 25
+}
+
+//BUCLE
+setInterval(loop,1000/FPS)
+function loop() {
+    contexto.clearRect(0,0,300,530)
+
+    //FONDO
+    contexto.drawImage(background,0,0)
+    contexto.drawImage(suelo,0,contexto.canvas.height - suelo.height)
+    //PERSONAJE   
+    contexto.drawImage(bird,personaje.x,personaje.y)
+
+    //TUBERIAS
+    personaje.y += gravedad
+}
+
+window.addEventListener("keydown",presionar)
