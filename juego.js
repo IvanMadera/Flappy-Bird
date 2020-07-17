e75a2001e89410bcd60c2238d0aececa43d823b9
Ivan Madera <ivan.madera1999@gmail.com>
2020-07-16 19:45:17 -0500

    Segundo avance del curso

--- juego.js
+++ juego.js
@@ -1,6 +1,9 @@
 var contexto = document.getElementById("lienzoJuego").getContext("2d")
 contexto.canvas.width = 300
 contexto.canvas.height = 530
+
+//VARIABLES
+var score = 0
 var FPS = 60
 var gravedad = 1.5
 var personaje = {
@@ -9,6 +12,14 @@ var personaje = {
     w:50,
     h:50
 }
+var tuberias = new Array()
+tuberias[0] = {
+    x:contexto.canvas.width,
+    y:0
+}
+//AUDIOS
+var punto = new Audio()
+punto.src = "audios/punto.mp3"
 
 //IMAGENES
 var bird = new Image()
@@ -43,7 +54,39 @@ function loop() {
     contexto.drawImage(bird,personaje.x,personaje.y)
 
     //TUBERIAS
+    for(var i = 0; i < tuberias.length; i++){
+        var constante = tuberiaNorte.height + 100
+        contexto.drawImage(tuberiaNorte,tuberias[i].x,tuberias[i].y)
+        contexto.drawImage(tuberiaSur,tuberias[i].x,tuberias[i].y + constante)
+        tuberias[i].x--
+        if(tuberias[i].y + tuberiaNorte.height < 80){
+            tuberias[i].y = 0    
+        }
+        if(tuberias[i].x == 150){
+            tuberias.push({
+                x:contexto.canvas.width,
+                y:Math.floor(Math.random()*tuberiaNorte.height) - tuberiaNorte.height
+            })
+        }
+        //COLISIONES
+        if(personaje.x + bird.width >= tuberias [i].x &&
+            personaje.x <= tuberias[i].x + tuberiaNorte.width &&
+            (personaje.y <= tuberias[i].y + tuberiaNorte.height ||
+                personaje.y + bird.height >= tuberias[i].y + constante)
+                || personaje.y + bird.height >= contexto.canvas.height - suelo.height){
+            //location.reload() 
+        }
+        if(tuberias[i].x == personaje.x){
+            score++
+            //punto.play()
+        }
+    }
+    //CONDICIONES
     personaje.y += gravedad
+    contexto.fillStyle = "rgba(0,0,0,1)"
+    contexto.font = "25px Arial"
+    contexto.fillText("Score: "+score,10,contexto.canvas.height-40)
 }
 
+//EVENTOS
 window.addEventListener("keydown",presionar)
